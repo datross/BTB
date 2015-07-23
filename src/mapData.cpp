6e3c4f87718b31342cfa697d3a737520eac95164
141592653 <julien.rixte@mailoo.org>
2015-07-23 19:28:12 +0200

    Now, audio works.

--- src/mapData.cpp
+++ src/mapData.cpp
@@ -1,10 +1,11 @@
 #include "mapData.hpp"
 #include <fstream>
 #include <sstream>
+#include <iostream>
 
 using namespace std;
 
-MapData::MapData(string file)
+MapData::MapData(const string& file)
 {
   ifstream json_file(file);
   if (!json_file)
@@ -13,6 +14,22 @@ MapData::MapData(string file)
   ss << json_file.rdbuf();
   string str_json = ss.str();
   json json_map = json::parse(str_json);
-  cout<<json_map["music_file"]<<endl;
   
+  cout<<json_map["music_file"]<<endl;
+  song = new sf::Music;
+  openSong("music/1vs0_JuniorGroove.ogg");
+  song->play();
+}
+
+void MapData::openSong(const string& file)
+{
+  cout<<file<<endl;
+  cout<<song->openFromFile(file)<<endl;
+  if(!song->openFromFile(file))
+    throw runtime_error("The loading of the music file " + file + "has failed.");
+}
+
+MapData::~MapData()
+{
+  delete song;
 }
--- src/mapData.hpp
+++ src/mapData.hpp
@@ -4,16 +4,19 @@
 #include "json.hpp"
 #include <vector>
 #include <string>
+#include <SFML/Audio.hpp>
 
 using json = nlohmann::json;
 
 class MapData
 {
 public :
-  MapData(std::string file);
+  MapData(const std::string& file);
+  void openSong(const std::string& file);
+  ~MapData();
 
 private :
-  
+  sf::Music* song; //maybe could be better to use RAII
 
 };
 
