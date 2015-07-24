c7aa817b1c565bc44f0f2936e19fed661f838283
141592653 <julien.rixte@mailoo.org>
2015-07-24 10:01:07 +0200

    Added the function loadSceneData. The loading of the JSON file is for the momen finished.

--- src/mapData.cpp
+++ src/mapData.cpp
@@ -15,18 +15,39 @@ MapData::MapData(const string& file)
   string str_json = ss.str();
   json json_map = json::parse(str_json);
   
-  cout<<json_map["music_file"]<<endl;
   song = new sf::Music;
-  openSong("music/1vs0_JuniorGroove.ogg");
-  song->play();
+  openSong(json_map["music_file"]);
+
+  loadSceneData(json_map);
 }
 
 void MapData::openSong(const string& file)
 {
-  cout<<file<<endl;
-  cout<<song->openFromFile(file)<<endl;
   if(!song->openFromFile(file))
-    throw runtime_error("The loading of the music file " + file + "has failed.");
+    throw runtime_error("The loading of the music file \"" + file + "\" has failed.");
+  
+}
+
+void MapData::loadSceneData(json json_map)
+{
+  try
+    {
+      json json_timeline = json_map["game_timeline"];
+      sceneElementInfo info_tmp;
+      for(json::iterator it = json_timeline.begin(); it != json_timeline.end(); ++it)
+	{
+	  info_tmp.time = (*it)["time"];
+	  info_tmp.duration = (*it)["duration"];
+	  info_tmp.type = (*it)["type"].get<string>();
+	  sceneData.push_back(info_tmp);
+	}
+    }
+  catch(exception const& e)
+    {
+      cerr<<"Error :" <<e.what()<<endl;
+      cerr<<"It seems that the json file loading has failed."<<endl;
+    }
+ 
 }
 
 MapData::~MapData()
--- src/mapData.hpp
+++ src/mapData.hpp
@@ -8,15 +8,25 @@
 
 using json = nlohmann::json;
 
+//this structure gives informations about a screenElement
+typedef struct sceneElementInfo
+{
+  double duration; 
+  double time;
+  std::string type;
+} sceneElementInfo;
+
 class MapData
 {
 public :
   MapData(const std::string& file);
   void openSong(const std::string& file);
+  void loadSceneData(json json_map);
   ~MapData();
 
 private :
   sf::Music* song; //maybe could be better to use RAII
+  std::vector<sceneElementInfo> sceneData;
 
 };
 
